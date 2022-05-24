c0416916205492d0565cda577c0cbf196fdc4128
Fighter28 <zhengzhinan28@gmail.com>
2022-05-24 17:30:49 +0800

    封装TopNav组件

---
+++ src/components/TopNav.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="topNav">
+    <div class="logo">LOGO</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
+  </div>
+</template>
+<style lang="scss" scoped>
+.topNav {
+  display: flex;
+  background: pink;
+  padding: 16px;
+
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+
+  > .menu {
+    display: flex;
+    flex-wrap: nowrap;
+    white-space: nowrap;
+    justify-content: center;
+    align-items: center;
+
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
+</style>
+<script lang="ts">
+export default {};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <div class="topNav">
-      <div class="logo">LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-    </div>
+    <TopNav/>
     <div class="banner">
       <h1>轱辘UI</h1>
       <h2>一个厉害的UI框架</h2>
@@ -18,23 +12,11 @@
   </div>
 </template>
 <style lang="scss" scoped>
-.topNav {
-  display: flex;
-  background: pink;
-  padding: 16px;
-  > .logo{
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu{
-    display: flex;
-    flex-wrap:nowrap;
-    white-space: nowrap;
-    justify-content: center;
-    align-items: center;
-    > li{
-      margin:0 1em;
-    }
-  }
-}
 </style>
+<script lang="ts">
+import TopNav from '../components/TopNav.vue';
+
+export default {
+  components: {TopNav}
+};
+</script>
