d1f267a0f99c09dc1ded7e58305cc7a8fd008dce
Fighter28 <zhengzhinan28@gmail.com>
2022-05-24 15:36:57 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    hello world
+  </div>
+  <justice/>
+</template>
+
+<script>
+import Justice from './components/Justice.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Justice
+  }
+}
+</script>
