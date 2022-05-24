bdd742bbb8085154771533b211ad8f7fd8e3baad
Fighter28 <zhengzhinan28@gmail.com>
2022-05-24 17:48:35 +0800

    Home页面的css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,6 +12,28 @@
   </div>
 </template>
 <style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: lightgreen;
+
+  > .actions {
+    padding: 8px 0;
+
+    a {
+      margin: 0 8px;
+      background-color: #fff;
+      display: inline-block;
+      border-radius: 14px;
+      height: 28px;
+      line-height: 28px;
+      padding:0 8px
+    }
+  }
+}
 </style>
 <script lang="ts">
 import TopNav from '../components/TopNav.vue';
